b0e2e7dac8224b1c0fc95bd65da921790fbe996f
Christoph Wottawa <christoph.wottawa@me.com>
2020-03-25 22:17:59 +0100

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Christoph Wottawa on 25.03.20.
+//  Copyright © 2020 Christoph Wottawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
