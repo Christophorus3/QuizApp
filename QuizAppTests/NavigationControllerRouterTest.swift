52f94a60b4a7fcfebd204925957894bde4f073c8
Christoph Wottawa <christoph.wottawa@me.com>
2020-11-24 23:13:37 +0100

    MOD navigation controller router and tests

--- QuizAppTests/NavigationControllerRouterTest.swift
+++ QuizAppTests/NavigationControllerRouterTest.swift
@@ -13,22 +13,52 @@ import QuizEngine
 
 class NavigationControllerRouterTest: XCTestCase {
     
-    func testRouteToQuestionPresentsQuestion() {
-        let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController)
-        
-        sut.routeTo(question: "Q1", answerCallback: {_ in })
-        
-        XCTAssertEqual(navigationController.viewControllers.count, 1)
-    }
-    
-    func testRouteToQuestionTwicePresentsQuestion() {
-        let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController)
+    let navigationController = NonAnimatedNavigationController()
+    let factory = ViewControllerFactoryStub()
+    lazy var sut = NavigationControllerRouter(navigationController, factory: factory)
+
+    func testRouteToQuestionTwicePresentsQuestionWithVCFactory() {
+        let viewController = UIViewController()
+        let secondViewController = UIViewController()
+
+        factory.stub(question:"Q1", with:viewController)
+        factory.stub(question:"Q2", with:secondViewController)
         
         sut.routeTo(question: "Q1", answerCallback: {_ in })
         sut.routeTo(question: "Q2", answerCallback: {_ in })
-        
+
         XCTAssertEqual(navigationController.viewControllers.count, 2)
+        XCTAssertEqual(navigationController.viewControllers.first, viewController)
+        XCTAssertEqual(navigationController.viewControllers.last, secondViewController)
+    }
+    
+    func testRouteToQuestionTwicePresentsQuestionWithRightCallback() {
+        var callbackFired = false
+        sut.routeTo(question: "Q1", answerCallback: { _ in callbackFired = true })
+        factory.stubbedCallbacks["Q1"]!("")
+
+        XCTAssertTrue(callbackFired)
+    }
+}
+
+class NonAnimatedNavigationController: UINavigationController {
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        super.pushViewController(viewController, animated: false)
+    }
+}
+
+class ViewControllerFactoryStub: ViewControllerFactory {
+
+    private var stubbedVCs = [String: UIViewController]()
+    var stubbedCallbacks = [String: (String) -> Void]()
+
+    
+    func stub(question: String, with viewController: UIViewController) {
+        stubbedVCs[question] = viewController
+    }
+    
+    func questionViewController(for question: String, answerCallback: @escaping (String) -> Void) -> UIViewController {
+        self.stubbedCallbacks[question] = answerCallback
+        return stubbedVCs[question] ?? UIViewController()
     }
 }
